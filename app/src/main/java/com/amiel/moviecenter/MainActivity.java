a16edc0530b2fceaf3c74924afb7efb57d15ae0b
AmielRe <amieli1999@gmail.com>
2022-11-22 18:08:28 +0200

    Add basic search view.
    Add basic search view inside menu.

--- app/src/main/java/com/amiel/moviecenter/MainActivity.java
+++ app/src/main/java/com/amiel/moviecenter/MainActivity.java
@@ -1,14 +1,89 @@
 package com.amiel.moviecenter;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    // List View object
+    ListView listView;
+
+    // Define array adapter for ListView
+    ArrayAdapter<String> adapter;
+
+    // Define array List for List View data
+    ArrayList<String> mylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialise ListView with id
+        listView = findViewById(R.id.listView);
+
+        // Add items to Array List
+        mylist = new ArrayList<>();
+        mylist.add("1");
+        mylist.add("11");
+        mylist.add("12");
+        mylist.add("22");
+        mylist.add("1234");
+        mylist.add("43");
+
+        // Set adapter to ListView
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu with items using MenuInflator
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        // Initialise menu item search bar
+        // with id and take its object
+        MenuItem searchViewItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchViewItem);
+
+        // attach setOnQueryTextListener
+        // to search view defined above
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // Override onQueryTextSubmit method which is call when submit query is searched
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // If the list contains the search query than filter the adapter
+                // using the filter method with the query as its argument
+                if (mylist.contains(query)) {
+                    adapter.getFilter().filter(query);
+                } else {
+                    // Search query not found in List View
+                    Toast.makeText(MainActivity.this, "Not found", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            // This method is overridden to filter the adapter according
+            // to a search query when the user is typing search
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
