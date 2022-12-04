e8ba1fceadd8303b5041187b90cdc5b411faaf60
AmielRe <amieli1999@gmail.com>
2022-12-04 17:59:29 +0200

    Check if user already signed in.

--- app/src/main/java/com/amiel/moviecenter/SplashScreenActivity.java
+++ app/src/main/java/com/amiel/moviecenter/SplashScreenActivity.java
@@ -5,20 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Intent i;
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            i = new Intent(SplashScreenActivity.this, MainActivity.class);
+        } else {
+            i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        }
+
         new Handler().postDelayed(new Runnable() {
-            
             @Override
             public void run() {
                 // This method will be executed once the timer is over
-                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
             }
