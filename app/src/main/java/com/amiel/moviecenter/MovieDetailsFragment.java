e14389e9f0e23cd9743300d9e43e97f4a37b23e6
AmielRe <amieli1999@gmail.com>
2022-12-08 18:58:07 +0200

    Create movie details fragment.

---
+++ app/src/main/java/com/amiel/moviecenter/MovieDetailsFragment.java
@@ -0,0 +1,38 @@
+package com.amiel.moviecenter;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class MovieDetailsFragment extends Fragment {
+
+    TextView movieName;
+    TextView movieYear;
+    ImageView movieImage;
+
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.movie_details_fragment, parent, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        movieName = view.findViewById(R.id.movie_details_movie_name);
+        movieYear = view.findViewById(R.id.movie_details_movie_year);
+        movieImage = view.findViewById(R.id.movie_details_movie_image);
+
+        movieName.setText(getArguments().getString("name"));
+        movieYear.setText(getArguments().getString("year"));
+        movieImage.setImageResource(getArguments().getInt("image"));
+    }
+}
