d51f33991e48548d11a8675cea9043284aed16c1
AmielRe <amieli1999@gmail.com>
2022-12-17 15:51:01 +0200

    Refactor DB entities for Room library.

--- app/src/main/java/com/amiel/moviecenter/DB/Model/Movie.java
+++ app/src/main/java/com/amiel/moviecenter/DB/Model/Movie.java
@@ -1,18 +1,37 @@
 package com.amiel.moviecenter.DB.Model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Movies")
 public class Movie {
 
-    public long id;
-    public String name;
-    public int year;
-    public float rating;
-    public String plot;
-    public byte[] poster;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private long id;
 
-    public Movie()
-    {
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String name;
 
-    }
+    @NonNull
+    @ColumnInfo(name = "year")
+    private int year;
+
+    @NonNull
+    @ColumnInfo(name = "rating")
+    private float rating;
+
+    @NonNull
+    @ColumnInfo(name = "plot")
+    private String plot;
+
+    @NonNull
+    @ColumnInfo(name = "poster")
+    private byte[] poster;
 
     public Movie(String name, int year, float rating, String plot, byte[] poster, long id)
     {
@@ -22,5 +41,61 @@ public class Movie {
         this.plot = plot;
         this.poster = poster;
         this.id = id;
+    }
+
+    @Ignore
+    public Movie() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    @NonNull
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(@NonNull String plot) {
+        this.plot = plot;
+    }
+
+    @NonNull
+    public byte[] getPoster() {
+        return poster;
+    }
+
+    public void setPoster(byte[] poster) {
+        this.poster = poster;
     }
 }
--- app/src/main/java/com/amiel/moviecenter/DB/Model/User.java
+++ app/src/main/java/com/amiel/moviecenter/DB/Model/User.java
@@ -1,16 +1,29 @@
 package com.amiel.moviecenter.DB.Model;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Users")
 public class User {
 
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     public long id;
+
+    @NonNull
+    @ColumnInfo(name = "username")
     public String username;
+
+    @NonNull
+    @ColumnInfo(name = "email")
     public String email;
+
+    @NonNull
+    @ColumnInfo(name = "profileImage")
     public byte[] profileImage;
-
-    public User()
-    {
-
-    }
 
     public User(String username, String email, byte[] profileImage, long id)
     {
@@ -18,5 +31,46 @@ public class User {
         this.email = email;
         this.profileImage = profileImage;
         this.id = id;
+    }
+
+    @Ignore
+    public User()
+    {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public byte[] getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(@NonNull byte[] profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
     }
 }
