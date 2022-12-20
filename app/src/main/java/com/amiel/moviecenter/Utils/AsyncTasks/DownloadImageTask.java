7d7d662842df589e2d529267c0ce64ddc102fa06
AmielRe <amieli1999@gmail.com>
2022-12-20 22:49:06 +0200

    Create async tasks for downloading data with OkHttp.

---
+++ app/src/main/java/com/amiel/moviecenter/Utils/AsyncTasks/DownloadImageTask.java
@@ -0,0 +1,60 @@
+package com.amiel.moviecenter.Utils.AsyncTasks;
+
+import androidx.appcompat.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    WeakReference<ImageView> movieImage;
+    WeakReference<AlertDialog> loadingDialog;
+
+    public DownloadImageTask(ImageView movieImage, AlertDialog loadingDialog) {
+        this.movieImage = new WeakReference<>(movieImage);
+        this.loadingDialog = new WeakReference<>(loadingDialog);
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+
+        final OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(urls[0])
+                .build();
+
+        Response response = null;
+        Bitmap mIcon11 = null;
+        try {
+            response = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (response.isSuccessful()) {
+            try {
+                mIcon11 = BitmapFactory.decodeStream(response.body().byteStream());
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        if(result != null) {
+            movieImage.get().setImageBitmap(result);
+            movieImage.get().setBackground(null);
+        }
+        loadingDialog.get().dismiss();
+    }
+}
