c34124ddabcc5918b98887183d57fe87f72930f9
AmielRe <amieli1999@gmail.com>
2023-01-07 14:06:37 +0200

    Fix warnings.
    Fix date locale warning.
    Fix string resource warning.

--- app/src/main/java/com/amiel/moviecenter/UI/MyPosts/MyPostViewHolder.java
+++ app/src/main/java/com/amiel/moviecenter/UI/MyPosts/MyPostViewHolder.java
@@ -1,18 +1,18 @@
 package com.amiel.moviecenter.UI.MyPosts;
 
-import android.graphics.Color;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.amiel.moviecenter.R;
 import com.amiel.moviecenter.Utils.ImageUtils;
 import com.amiel.moviecenter.Utils.OnMyPostRowItemClickListener;
 import com.amiel.moviecenter.databinding.MyPostRowItemBinding;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MyPostViewHolder extends RecyclerView.ViewHolder{
     MyPostRowItemBinding binding;
@@ -20,17 +20,6 @@ public class MyPostViewHolder extends RecyclerView.ViewHolder{
     public MyPostViewHolder(@NonNull View itemView, OnMyPostRowItemClickListener listener) {
         super(itemView);
         binding = MyPostRowItemBinding.bind(itemView);
-
-//        itemView.setOnClickListener(view -> {
-//            int pos = getAdapterPosition();
-//            toggleEditText(binding.myPostRowItemPostText, true);
-//            binding.myPostRowItemPostText.setOnFocusChangeListener((view1, inFocus) -> {
-//                if(!inFocus) {
-//                    toggleEditText(binding.myPostRowItemPostText, false);
-//                    listener.onItemClick(pos, binding.myPostRowItemPostText.getText().toString());
-//                }
-//            });
-//        });
 
         binding.myPostRowItemPostText.setOnFocusChangeListener((view1, inFocus) -> {
             int pos = getAdapterPosition();
@@ -44,9 +33,9 @@ public class MyPostViewHolder extends RecyclerView.ViewHolder{
     public void bind(MyPostRowItem post, int pos) {
         binding.myPostRowItemPostRating.setRating(post.post.getRating());
         binding.myPostRowItemPostText.setText(post.post.getText());
-        binding.myPostRowItemMovieNameAndYear.setText(post.postMovie.getName() + " (" + post.postMovie.getYear() + ")");
+        binding.myPostRowItemMovieNameAndYear.setText(this.itemView.getContext().getString(R.string.movie_name_and_year, post.postMovie.getName(), post.postMovie.getYear()));
         binding.myPostRowItemPostImage.setImageBitmap(ImageUtils.getBitmap(post.post.getImage()));
-        binding.myPostRowItemPostDate.setText(new SimpleDateFormat("dd-MM-yyyy HH:mm").format(post.post.postDate));
+        binding.myPostRowItemPostDate.setText(new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault()).format(post.post.postDate));
     }
 
     private void toggleEditText(EditText editText, boolean isEnabled) {
--- app/src/main/java/com/amiel/moviecenter/Utils/Converters.java
+++ app/src/main/java/com/amiel/moviecenter/Utils/Converters.java
@@ -5,11 +5,12 @@ import androidx.room.TypeConverter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Converters {
     @TypeConverter
     public static Date fromTimestamp(Long value) throws ParseException {
-        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
         return value == null ? null : df.parse(df.format(new Date(value)));
     }
 
